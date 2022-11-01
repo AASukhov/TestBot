67f2d5c84754a7d39dc0382bf8422a03ad3eb88a
Andrey <arkhonfills@list.ru>
2022-11-01 22:10:18 +0300

    Menu added

--- src/main/java/com/example/testbot/service/TestBotService.java
+++ src/main/java/com/example/testbot/service/TestBotService.java
@@ -1,12 +1,26 @@
 package com.example.testbot.service;
 
 import com.example.testbot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Component
 public class TestBotService extends TelegramLongPollingBot {
 
@@ -14,6 +28,17 @@ public class TestBotService extends TelegramLongPollingBot {
 
     public TestBotService(BotConfig config) {
         this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -35,6 +60,9 @@ public class TestBotService extends TelegramLongPollingBot {
                 case "/start":
                         startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                         break;
+                case "/help":
+                        sendMessage(chatId, "Sorry, that point will be made soon");
+                        break;
                 default: sendMessage(chatId, "Sorry, command was no recognized");
             }
         }
@@ -42,6 +70,7 @@ public class TestBotService extends TelegramLongPollingBot {
 
     private void startCommandReceived(long chatId, String name){
         String answer = "Hello, " + name + ", nice to meet you!";
+        log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
 
@@ -53,7 +82,7 @@ public class TestBotService extends TelegramLongPollingBot {
         try{
             execute(msg);
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            log.error("Error occured " + e.getMessage());
         }
     }
 }
